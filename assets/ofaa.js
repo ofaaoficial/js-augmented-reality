25f44b79d7f0154820648f8b93b95b5e884b53c3
Oscar Amado <oscarfamado@gmail.com>
2020-05-13 13:19:23 -0500

    ejemplos de AR

---
+++ assets/ofaa.js
@@ -0,0 +1,133 @@
+
+let scene, camera, renderer, clock, deltaTime, totalTime;
+let arToolkitSource, arToolkitContext;
+let markerRoot1;
+let mesh1;
+
+initialize();
+animate();
+
+function initialize()
+{
+    scene = new THREE.Scene();
+
+    let ambientLight = new THREE.AmbientLight( 0xcccccc, 1.0 );
+    scene.add( ambientLight );
+
+    camera = new THREE.Camera();
+    scene.add(camera);
+
+    renderer = new THREE.WebGLRenderer({
+        antialias : true,
+        alpha: true
+    });
+    renderer.setClearColor(new THREE.Color('lightgrey'), 0)
+    renderer.setSize( 640, 480 );
+    renderer.domElement.style.position = 'absolute'
+    renderer.domElement.style.top = '0px'
+    renderer.domElement.style.left = '0px'
+    document.body.appendChild( renderer.domElement );
+
+    clock = new THREE.Clock();
+    deltaTime = 0;
+    totalTime = 0;
+
+    ////////////////////////////////////////////////////////////
+    // setup arToolkitSource
+    ////////////////////////////////////////////////////////////
+
+    arToolkitSource = new THREEx.ArToolkitSource({
+        sourceType : 'webcam',
+    });
+
+    function onResize()
+    {
+        arToolkitSource.onResize()
+        arToolkitSource.copySizeTo(renderer.domElement)
+        if ( arToolkitContext.arController !== null )
+        {
+            arToolkitSource.copySizeTo(arToolkitContext.arController.canvas)
+        }
+    }
+
+    arToolkitSource.init(function onReady(){
+        onResize()
+    });
+
+    // handle resize event
+    window.addEventListener('resize', function(){
+        onResize()
+    });
+
+    ////////////////////////////////////////////////////////////
+    // setup arToolkitContext
+    ////////////////////////////////////////////////////////////
+
+    // create atToolkitContext
+    arToolkitContext = new THREEx.ArToolkitContext({
+        cameraParametersUrl: 'data/camera_para.dat',
+        detectionMode: 'mono'
+    });
+
+    // copy projection matrix to camera when initialization complete
+    arToolkitContext.init( function onCompleted(){
+        camera.projectionMatrix.copy( arToolkitContext.getProjectionMatrix() );
+    });
+
+    ////////////////////////////////////////////////////////////
+    // setup markerRoots
+    ////////////////////////////////////////////////////////////
+
+    // build markerControls
+    markerRoot1 = new THREE.Group();
+    scene.add(markerRoot1);
+    let markerControls1 = new THREEx.ArMarkerControls(arToolkitContext, markerRoot1, {
+        type: 'pattern', patternUrl: "data/hiro.patt",
+    })
+
+    let geometry1 = new THREE.PlaneBufferGeometry(1,1, 4,4);
+    let loader = new THREE.TextureLoader();
+    // let texture = loader.load( 'images/earth.jpg', render );
+    let material1 = new THREE.MeshBasicMaterial( { color: 0x0000ff, opacity: 0.5 } );
+    mesh1 = new THREE.Mesh( geometry1, material1 );
+    mesh1.rotation.x = -Math.PI/2;
+    markerRoot1.add( mesh1 );
+
+    function onProgress(xhr) { console.log( (xhr.loaded / xhr.total * 100) + '% loaded' ); }
+    function onError(xhr) { console.log( 'An error happened' ); }
+
+    new THREE.MTLLoader()
+        .setPath( 'models/fish/' )
+        .load( 'fish-2.mtl', function ( materials ) {
+            materials.preload();
+            new THREE.OBJLoader()
+                .setMaterials( materials )
+                .setPath( 'models/fish/' )
+                .load( 'fish-2.obj', function ( group ) {
+                    group.children.forEach((mesh) => {
+                        mesh.material.side = THREE.DoubleSide;
+                        mesh.position.y = 0.25;
+                        mesh.scale.set(0.25,0.25,0.25);
+                        markerRoot1.add(mesh);
+                    });
+                }, onProgress, onError );
+        });
+}
+
+function update()
+{
+    // update artoolkit on every frame
+    if ( arToolkitSource.ready !== false )
+        arToolkitContext.update( arToolkitSource.domElement );
+}
+
+function render() { renderer.render( scene, camera ); }
+
+function animate()
+{
+    requestAnimationFrame(animate);
+    deltaTime = clock.getDelta();
+    totalTime += deltaTime;
+    update();
+    render();
+}
